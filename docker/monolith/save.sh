cd1c3ce7107440365994772807c133860f29b04b
hkjang <gagagiga@naver.com>
2026-01-09 14:15:46 +0900

    fix save sh

--- docker/monolith/save.sh
+++ docker/monolith/save.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Saving image to jasca-offline.tar..."
 docker save jasca-offline -o jasca-offline.tar
+tar cvfz jasca.tar.gz jasca-offline.tar
 echo "Done. You can transfer jasca-offline.tar to your offline environment."
